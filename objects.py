a6cafe59e13a8d11f45e9496f9d4db952c9f2727
Omowumiishola <105393329+Omowumiishola@users.noreply.github.com>
2022-05-29 00:18:51 +0100

    Add files via upload

---
+++ objects.py
@@ -0,0 +1,18 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  6 2014, 22:16:31) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self):
+        pass
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+ Kunle = student(name)
